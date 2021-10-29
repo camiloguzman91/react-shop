ed7ad982e39d0254772572d908176c83537cd154
Camilo Guzman <camiloge_91@hotmail.com>
2021-10-28 23:28:06 -0500

    routes_nav

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,23 +1,25 @@
 //imr
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
 import Login from '../containers/Login';
 import RecoveryPassword from '../containers/RecoveryPassword';
+import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
 import '../styles/global.css';
 
 //slr
 const App = () => {
   return(
     <BrowserRouter>
-      <Switch>
-        <Layout>
+      <Layout>
+        <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/recovery-password" component={RecoveryPassword} />
-          <Route component={NotFound} />
-        </Layout>
-      </Switch>
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Layout>
     </BrowserRouter>
   );
 }
