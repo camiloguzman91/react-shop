d08f01050358d02b6d9c4b809fe555a054150abd
Camilo Guzman <camiloge_91@hotmail.com>
2021-11-18 21:57:29 -0500

    context

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../styles/ProductItem.scss';
+import AppContext from '../context/AppContext';
+import addToCartImage from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = ({ product }) => {
+	const { addToCart } = useContext(AppContext);
+
+	const handleClick = item => {
+		addToCart(item);
+	}
+
 	return (
 		<div className="ProductItem">
 			<img src={product.images[0]} alt={product.title} />
@@ -10,8 +18,8 @@ const ProductItem = ({ product }) => {
 					<p>${product.price}</p>
 					<p>{product.title}</p>
 				</div>
-				<figure>
-					<img src="./icons/bt_add_to_cart.svg" alt="" />
+				<figure onClick={() => handleClick(product)}>
+					<img src={addToCartImage} alt="cart" />
 				</figure>
 			</div>
 		</div>
